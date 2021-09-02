90482abb0c5b617cb87b94a7939a6bcad8610f09
Rushi5078 <88040056+Rushi5078@users.noreply.github.com>
2021-09-02 21:21:22 +0530

    Add : initial commit

---
+++ Vowel or Consonant.java
@@ -0,0 +1,23 @@
+package Alphabhet;
+import java.util.Scanner;
+
+public class VowelorCosonant {
+	public static void main(String[] args) {
+		//decleration part
+		String  alphabhet;
+		String  a,e,i,o,u;
+		Scanner sc = new Scanner(System.in);
+		//user input
+		System.out.println("Enter the aphabhet");
+		alphabhet = sc.next();
+		//execution part
+		if(alphabhet == a && e && i && o && u )
+		{
+			System.out.println("this is vowel");
+		}
+		else
+		{
+			System.out.println("this is consonant");
+			
+		}
+	}
